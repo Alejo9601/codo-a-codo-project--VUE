5ffb79ed7905c7029e7c8cf8c37e951d4d67c855
Alejo9601 <alejandro96jm@gmail.com>
2022-11-02 17:37:19 -0300

    Added VUE on created

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 import startCountdown from "./countdownController.js";
 import startCardSlider from "./cardSliderController.js";
-import loadQuickAccessProducts from "./tabMenuController.js";
-import paintNewProducts from "./newProductsController.js";
 
-loadQuickAccessProducts();
-paintNewProducts();
 startCountdown();
 startCardSlider();
